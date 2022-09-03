ef947424ad6696cfa65a9db7fe5c3634c2f808a1
raiihan <s.siilavn@yahoo.com>
2022-09-04 00:24:24 +0600

    engine version change

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const jsonServer = require('json-server')
 const server = jsonServer.create()
-const router = jsonServer.router('productDB.json')
+const router = jsonServer.router('./productDB.json')
 const middlewares = jsonServer.defaults()
 
 server.use(middlewares)
